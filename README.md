3afe622a0262592fb62bd9005fd4e3a04cce6e50
SanuliGehara <geharasanuli5@gmail.com>
2024-06-18 13:57:59 +0530

    Update files

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# My-Contacts-App
+# MyContacts App
+
+MyContacts App is a simple JavaScript application to manage your contacts. The app allows users to add, search, and display contacts dynamically.
+
+## Features
+
+- **Search Contacts:** Easily search through your contacts by typing a name in the search bar.
+
+- **Add New Contact:** Click the plus button to add a new contact. Enter the contact's name and phone number, then save it to your list.
+
+- **Dynamic UI:** The contact list updates in real-time as you add or search contacts.
+
+## Screenshots
+
+![image1](https://github.com/SanuliGehara/My-Contacts-App/assets/154311325/e885bc86-b9b0-497c-96ad-192d125681ac)
 
 
+![image2](https://github.com/SanuliGehara/My-Contacts-App/assets/154311325/2549310b-6a01-41e8-ab78-09c4fdadfe64)
--- main.js
+++ main.js
@@ -7,8 +7,8 @@ filterInput.addEventListener('keyup', filterNames);
 // Get the add contact button
 let plusButton = document.getElementById('add-contact');
 
-// Adding New contact  
-plusButton.addEventListener('click',  addContact);
+// Add eventListner to plus button 
+plusButton.addEventListener('click', createNewContact);
 
 function filterNames(e) {
     // Get the entered value from filterInput
@@ -29,12 +29,98 @@ function filterNames(e) {
     })
 }
 
-function addContact() {
-    console.log('new contact added!');
+function createNewContact() {
+    // Hide contact name-list
+    let namesList = document.querySelector('.names');
+    namesList.style.display = 'none';
 
-    // HIde name list
     // appear new div asking the contact details
-    // after clicking ok, add it to contact list 
-    // display contact list
+    let newDiv = document.createElement('div');
+    newDiv.className = 'container mt-3 p-5 bg-secondary newContact';
+    newDiv.setAttribute("style", "width:75%;");
 
+    // Create an input field for name
+    let name = document.createElement('input');
+    name.setAttribute("type", "text");
+    name.className = 'form-control mb-4 contact-name';
+    name.setAttribute("placeholder", "Name");
+
+    // Create an input field for phoneNumber
+    let phoneNumber = document.createElement('input');
+    name.setAttribute("type", "text");
+    phoneNumber.className = 'form-control mb-3 contact-phoneNumber';
+    phoneNumber.setAttribute("placeholder", "Phone Number");
+
+    // Create a button 
+    let confirmBtn = document.createElement('button');
+    confirmBtn.className = 'btn btn-primary mt-3';
+    confirmBtn.textContent = 'Save';
+
+    // Append elements to newDiv container
+    newDiv.append(name, phoneNumber, confirmBtn);
+
+    // insert newDiv in to DOM
+    let searchBar = document.querySelector('.input-group');
+    searchBar.after(newDiv);
+
+    // after clicking save, add contactName to contact list
+    confirmBtn.addEventListener('click', addContactName);
+}
+
+function addContactName() {
+    let name = document.querySelector('.contact-name').value;  //get input name
+    name = name[0].toUpperCase() + name.substring(1);
+
+    const letterList = document.getElementsByClassName('letter'); // Get letters list 
+    let letterExists = false;
+
+    // Loop through the letterList array
+    Array.from(letterList).forEach(function (letter) {
+        // check if letter bar exists
+        if (name[0] === letter.textContent) {
+            letterExists = true;
+            
+            // create new li element
+            let li = document.createElement('li');
+            li.className = 'list-group-item item';
+            li.textContent = name;
+
+            // Add to the contact-List
+            letter.after(li);
+
+            // Update UI
+            updateUI();
+        }
+    });
+
+    // If letter does not exists in the letterList, 
+    if (!letterExists) {
+        // Create a new letter bar
+        let newLetterBar = document.createElement('li');
+        newLetterBar.className = 'list-group-item list-group-item-action active letter';
+        newLetterBar.textContent = name[0];
+
+         // create new li element for the contact
+        let newContact = document.createElement('li');
+        newContact.className = 'list-group-item item';
+        newContact.textContent = name;
+
+        // Add the new letter bar and contact to the correct position
+        let namesList = document.querySelector('.names');
+        namesList.append(newLetterBar);
+        newLetterBar.after(newContact);
+
+        // Update UI
+        updateUI();
+    }
+};
+
+function updateUI() {
+    // Hide new contact box
+    let newContactBox = document.querySelector('.newContact');
+    newContactBox.style.display = 'none';
+
+    // Display contact name-list
+    let namesList = document.querySelector('.names');
+    namesList.style.display = 'block';
 }
